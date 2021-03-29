cfc0b1585dee3dc42bf0d2a223206460b5b303fb
alexoreiro <alexis.oreiro@icloud.com>
2021-03-29 17:20:44 +0000

    setup mysql changes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,7 @@
 -- Prepares MySQL server for the project
 
 CREATE DATABASE IF NOT EXIST hbnb_dev_db;
-CREATE USER IF NOT EXIST hbnb_dev@localhost IDENTIFIED by 'hbnb_dev_pwd';
-USE hbnb_dev_db;
+CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+
